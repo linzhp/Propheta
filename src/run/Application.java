3c4729ee052b13e5ce21e33cbbcc700251a28f78
linzhp <lin.zhp@gmail.com>
2009-10-16 11:12:57 +0800

    initial commit

---
+++ src/run/Application.java
@@ -0,0 +1,40 @@
+package run;
+
+import org.eclipse.jface.action.MenuManager;
+import org.eclipse.jface.window.ApplicationWindow;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.Shell;
+
+public class Application extends ApplicationWindow {
+
+	public Application() {
+		super(null);
+	}
+
+	public void run() {
+		setBlockOnOpen(true);
+		open();
+		Display.getCurrent().dispose();
+	}
+
+	protected Control createContents(Composite parent) {
+		Shell shell=parent.getShell();
+		MenuManager fileMenuManager = new MenuManager("File");
+		Menu menuBar = new Menu(shell, SWT.BAR);
+		fileMenuManager.fill(menuBar, -1);
+		shell.setMenuBar(menuBar);
+		
+		Label label = new Label(parent, SWT.CENTER);
+		label.setText("Hello, World");
+		return label;
+	}
+	
+	public static void main(String[] args) {
+		new Application().run();
+	}
+}
