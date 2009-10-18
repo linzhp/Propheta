2193c42ae30855a28274d8ce9cfd3860f41eade6
林中鹏 <clive@nymph.(none)>
2009-10-18 16:09:51 +0800

    run the wizard

--- src/run/Application.java
+++ src/run/Application.java
@@ -1,5 +1,7 @@
 package run;
 
+import newProject.NewProjectAction;
+
 import org.eclipse.jface.action.MenuManager;
 import org.eclipse.jface.window.ApplicationWindow;
 import org.eclipse.swt.SWT;
@@ -7,15 +9,14 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Menu;
-import org.eclipse.swt.widgets.Shell;
 
-import actions.NewProjectAction;
 
 public class Application extends ApplicationWindow {
 
+
 	public Application() {
 		super(null);
+		addMenuBar();
 	}
 
 	public void run() {
@@ -24,17 +25,26 @@ public class Application extends ApplicationWindow {
 		Display.getCurrent().dispose();
 	}
 
+	@Override
 	protected Control createContents(Composite parent) {
-		Shell shell=parent.getShell();
-		MenuManager fileMenuManager = new MenuManager("File");
-		fileMenuManager.add(new NewProjectAction());
-		Menu menuBar = new Menu(shell, SWT.BAR);
-		fileMenuManager.fill(menuBar, -1);
-		shell.setMenuBar(menuBar);
 		 
 		Label label = new Label(parent, SWT.CENTER);
 		label.setText("Hello, World");
-		return label;
+		
+		parent.setSize(1024, 768);
+//		parent.pack();
+		return parent;
+	}
+	
+	@Override
+	protected MenuManager createMenuManager(){
+		MenuManager mainMenu = new MenuManager(null);
+		//File Menu
+		MenuManager fileMenuManager = new MenuManager("文件");
+		fileMenuManager.add(new NewProjectAction());
+		
+		mainMenu.add(fileMenuManager);
+		return mainMenu;
 	}
 	
 	public static void main(String[] args) {
