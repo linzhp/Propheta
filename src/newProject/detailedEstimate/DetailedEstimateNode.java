c94a34f81ea9e914140fdb7dbe75e26d8fe9a2a8
unknown <Hopone@.(none)>
2009-10-27 10:36:53 +0800

    finish dispose of wizard

--- src/newProject/detailedEstimate/DetailedEstimateNode.java
+++ src/newProject/detailedEstimate/DetailedEstimateNode.java
@@ -1,25 +1,30 @@
 package newProject.detailedEstimate;
 
-
 import org.eclipse.jface.wizard.IWizard;
 import org.eclipse.jface.wizard.IWizardNode;
 import org.eclipse.swt.graphics.Point;
 
 public class DetailedEstimateNode implements IWizardNode {
-	private static DetailedEstimateWizard wizard;
+	//用实例变量，防止内存泄漏
+	private DetailedEstimateWizard wizard;
 
+	/**
+	 * 这个方法会被WizardSelectionPage的dispose方法自动调用，
+	 * 而后者又会被它所在的Wizard.dispose()自动调用
+	 */
 	@Override
 	public void dispose() {
-		wizard=null;
+        if ( wizard != null )
+        {
+            wizard.dispose();
+            wizard = null;
+        }
 	}
 
-	@Override
 	public Point getExtent() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public IWizard getWizard() {
 		if(wizard==null)
 			wizard=new DetailedEstimateWizard();
