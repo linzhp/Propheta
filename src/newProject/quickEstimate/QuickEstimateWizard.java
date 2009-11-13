9c63ab85693fac08d8e0e7143f635679f746553b
林中鹏 <clive@nymph.(none)>
2009-11-14 00:33:18 +0800

    ajusting reslut layout in progress

--- src/newProject/quickEstimate/QuickEstimateWizard.java
+++ src/newProject/quickEstimate/QuickEstimateWizard.java
@@ -5,9 +5,14 @@ import java.util.ArrayList;
 import newProject.PIEstimate.FactorPage;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.layout.RowData;
+import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
 import org.jfree.chart.JFreeChart;
 import org.jfree.experimental.chart.swt.ChartComposite;
 
@@ -62,40 +67,46 @@ public class QuickEstimateWizard extends Wizard {
 
 			if(resultView != null)
 				resultView.dispose();
-			resultView = new Composite(displayArea, SWT.NONE);
-			resultView.setLayout(new GridLayout(1, false));
+			resultView = new Composite(displayArea, SWT.BORDER);
+			GridLayout layout = new GridLayout(1, false);
+			resultView.setLayout(layout);
+			GridData gData = new GridData();
+			gData.grabExcessHorizontalSpace = true;
+			gData.horizontalAlignment = GridData.HORIZONTAL_ALIGN_END;
 			
-			if (arrayPI.size() != 0) {
-				// 显示生产率的中位数
-				Double median = Statistic.getMedian(arrayPI);
-				Label PImedian = new Label(resultView, SWT.SINGLE);
-				PImedian.setText("生产率中位数值：" + median);
-
-				// 显示工作量的蒙特卡罗图
-				Double mean = Statistic.getMean(arrayPI);
-				Double standardDeviation = Statistic.getStandardDeviation(mean,
-						arrayPI);
-				JFreeChart monteCarloChart = LineChart
-						.createMonteCarloChart(LineChart
-								.createMonteCarloDataSet(getSize(), mean,
-										standardDeviation));
-				ChartComposite monteCarloFrame = new ChartComposite(resultView,
-						SWT.NONE, monteCarloChart, true);
-				monteCarloFrame.setSize(300,200);
-			} else {
-				Label noResult = new Label(resultView, SWT.SINGLE);
-				noResult.setText("没有搜索到任何相关历史数据，无法显示“生产率中位数值”与“工作量的蒙特卡罗图”");
-
-			}
-			// 显示规模相近的历史项目的工作量分布
-			JFreeChart effortChart = LineChart.createEffortChart(LineChart
-					.createEffortXYDataset(arrayEffort));
-			ChartComposite effortFrame = new ChartComposite(resultView, SWT.NONE,
-					effortChart, true);
-			effortFrame.setSize(300,200);
+			new Text(resultView, SWT.BORDER).setLayoutData(gData);
+//			new Composite(resultView, SWT.BORDER);
+//			new Composite(resultView, SWT.BORDER);
+			
+//			if (arrayPI.size() != 0) {
+//				// 显示生产率的中位数
+//				Double median = Statistic.getMedian(arrayPI);
+//				Label PImedian = new Label(resultView, SWT.SINGLE);
+//				PImedian.setText("生产率中位数值：" + median);
+//
+//				// 显示工作量的蒙特卡罗图
+//				Double mean = Statistic.getMean(arrayPI);
+//				Double standardDeviation = Statistic.getStandardDeviation(mean,
+//						arrayPI);
+//				JFreeChart monteCarloChart = LineChart
+//						.createMonteCarloChart(LineChart
+//								.createMonteCarloDataSet(getSize(), mean,
+//										standardDeviation));
+//				Composite monteCarloFrame = new ChartComposite(resultView,
+//						SWT.BORDER, monteCarloChart, true);
+//				monteCarloFrame.setLayoutData(gData);
+//			} else {
+//				Label noResult = new Label(resultView, SWT.SINGLE);
+//				noResult.setText("没有搜索到任何相关历史数据，无法显示“生产率中位数值”与“工作量的蒙特卡罗图”");
+//
+//			}
+//			// 显示规模相近的历史项目的工作量分布
+//			JFreeChart effortChart = LineChart.createEffortChart(LineChart
+//					.createEffortXYDataset(arrayEffort));
+//			ChartComposite effortFrame = new ChartComposite(resultView, SWT.NONE,
+//					effortChart, true);
 			
 			resultView.setBounds(displayArea.getClientArea());
-			resultView.setSize(800, 600);
 			
 			this.dispose();
 			return true;
--- src/run/Application.java
+++ src/run/Application.java
@@ -4,6 +4,7 @@ import newProject.NewProjectAction;
 
 import org.eclipse.jface.action.MenuManager;
 import org.eclipse.jface.window.ApplicationWindow;
+import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
@@ -24,7 +25,7 @@ public class Application extends ApplicationWindow {
 
 	protected Control createContents(Composite parent) {
 		Composite contentArea = (Composite)super.createContents(parent);
-		contentArea.setLayout(new GridLayout(1,false));//TODO 暂时设为FillLayout
+		contentArea.setLayout(new FillLayout());//TODO 暂时设为FillLayout
 		//TODO 以后需要时在这里加contentArea的子控件，也就是主窗口的内容
 		return contentArea;
 	}
