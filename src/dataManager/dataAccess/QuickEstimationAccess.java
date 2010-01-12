0134c6906d96271ff4076e39553ceb08ab7c9f0f
hezhimin <Administrator@.(none)>
2010-01-12 11:09:46 +0800

    QuickEstimationAccess.java
    Signed-off-by: hezhimin <>

--- src/dataManager/dataAccess/QuickEstimationAccess.java
+++ src/dataManager/dataAccess/QuickEstimationAccess.java
@@ -1,5 +1,11 @@
 package dataManager.dataAccess;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import dataManager.dataEntities.NodeBasicInformation;
+import dataManager.dataEntities.QuickEstimationRecord;
+
 /**
  * 数据访问类，用于quickEstimation表的存取操作
  * @author Administrator
@@ -7,4 +13,39 @@ package dataManager.dataAccess;
  */
 public class QuickEstimationAccess extends DataBaseAccess{
 
+	public QuickEstimationAccess(){
+		
+	}
+	
+	
+	public QuickEstimationAccess(String dataBasePath){
+		super(dataBasePath);
+	}
+	
+	
+	public QuickEstimationRecord getQuickEstimationByNodeID(int nodeID){
+		try {
+			String sqlString="select [estimationID],[nodeID],[dataType],[formulaEffort],[historyEffort],[meanProductivity]," +
+					"[stanDevProductivity] from quickEstimation where nodeID="+nodeID;
+			ResultSet rs=statement.executeQuery(sqlString);
+			if(rs.next()){
+				QuickEstimationRecord record=new QuickEstimationRecord();
+				record.setEstimationID(rs.getInt("estimationID"));
+				record.setNodeID(rs.getInt("nodeID"));
+				record.setDataType(rs.getString("dataType"));
+				record.setFormulaEffort(rs.getDouble("formulaEffort"));
+				record.setHistoryEffort(rs.getDouble("historyEffort"));
+				record.setMeanProductivity(rs.getDouble("meanProductivity"));
+				record.setStanDevProductivity(rs.getDouble("stanDevProductivity"));
+				
+				return record;
+			}else{
+				return null;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();			
+		}	
+		return null;
+	}
 }
+
