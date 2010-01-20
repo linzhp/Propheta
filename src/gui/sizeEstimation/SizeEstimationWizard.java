a84e0e8e50eddb0c9892854d21dbd287c8008498
林中鹏 <lin.zhp@gmail.com>
2010-01-20 09:54:36 +0800

    rename classes

--- src/gui/sizeEstimation/SizeEstimationWizard.java
+++ src/gui/sizeEstimation/SizeEstimationWizard.java
@@ -9,9 +9,9 @@ public class SizeEstimationWizard extends Wizard{
 	private SizeEstimationTypePage sizeEstimationTypePage;
 	private ManualSizeEstimationPage manualSizeEstimationPage;
 	private HistoricalDataBaseSizeEstimationPage historicalDataBaseSizeEstimationPage;
-	private CocomoSizeEstimation_newDeveloped_Page cocomoSizeEstimation_newDeveloped_Page;
-	private CocomoSizeEstimation_reused_Page cocomoSizeEstimation_reused_Page;
-	private CocomoSizeEstimation_maintained_Page cocomoSizeEstimation_maintained_Page;
+	private CocomoNewDevelopedPage cocomoSizeEstimation_newDeveloped_Page;
+	private CocomoReusedPage cocomoSizeEstimation_reused_Page;
+	private CocomoMaintainedPage cocomoSizeEstimation_maintained_Page;
 	
 	//用户输入
 	
@@ -45,30 +45,30 @@ public class SizeEstimationWizard extends Wizard{
 		this.historicalDataBaseSizeEstimationPage = historicalDataBaseSizeEstimationPage;
 	}
 
-	public CocomoSizeEstimation_newDeveloped_Page getCocomoSizeEstimation_newDeveloped_Page() {
+	public CocomoNewDevelopedPage getCocomoSizeEstimation_newDeveloped_Page() {
 		return cocomoSizeEstimation_newDeveloped_Page;
 	}
 
 	public void setCocomoSizeEstimation_newDeveloped_Page(
-			CocomoSizeEstimation_newDeveloped_Page cocomoSizeEstimationNewDevelopedPage) {
+			CocomoNewDevelopedPage cocomoSizeEstimationNewDevelopedPage) {
 		cocomoSizeEstimation_newDeveloped_Page = cocomoSizeEstimationNewDevelopedPage;
 	}
 
-	public CocomoSizeEstimation_reused_Page getCocomoSizeEstimation_reused_Page() {
+	public CocomoReusedPage getCocomoSizeEstimation_reused_Page() {
 		return cocomoSizeEstimation_reused_Page;
 	}
 
 	public void setCocomoSizeEstimation_reused_Page(
-			CocomoSizeEstimation_reused_Page cocomoSizeEstimationReusedPage) {
+			CocomoReusedPage cocomoSizeEstimationReusedPage) {
 		cocomoSizeEstimation_reused_Page = cocomoSizeEstimationReusedPage;
 	}
 
-	public CocomoSizeEstimation_maintained_Page getCocomoSizeEstimation_maintained_Page() {
+	public CocomoMaintainedPage getCocomoSizeEstimation_maintained_Page() {
 		return cocomoSizeEstimation_maintained_Page;
 	}
 
 	public void setCocomoSizeEstimation_maintained_Page(
-			CocomoSizeEstimation_maintained_Page cocomoSizeEstimationMaintainedPage) {
+			CocomoMaintainedPage cocomoSizeEstimationMaintainedPage) {
 		cocomoSizeEstimation_maintained_Page = cocomoSizeEstimationMaintainedPage;
 	}
 
@@ -86,9 +86,9 @@ public class SizeEstimationWizard extends Wizard{
 		sizeEstimationTypePage=new SizeEstimationTypePage("请选择估算方式");
 		manualSizeEstimationPage=new ManualSizeEstimationPage("人工输入");
 		historicalDataBaseSizeEstimationPage=new HistoricalDataBaseSizeEstimationPage("参考历史数据");
-		cocomoSizeEstimation_newDeveloped_Page=new CocomoSizeEstimation_newDeveloped_Page("新开发的代码");
-		cocomoSizeEstimation_reused_Page=new CocomoSizeEstimation_reused_Page("重用的代码");
-		cocomoSizeEstimation_maintained_Page=new CocomoSizeEstimation_maintained_Page("维护的代码");
+		cocomoSizeEstimation_newDeveloped_Page=new CocomoNewDevelopedPage("新开发的代码");
+		cocomoSizeEstimation_reused_Page=new CocomoReusedPage("重用的代码");
+		cocomoSizeEstimation_maintained_Page=new CocomoMaintainedPage("维护的代码");
 	
 		this.setWindowTitle("代码规模估算");
 		this.addPage(sizeEstimationTypePage);
