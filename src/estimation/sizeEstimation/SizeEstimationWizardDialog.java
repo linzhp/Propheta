0f45de760afc241899e1f58d27734a62e3b5e461
林中鹏 <lin.zhp@gmail.com>
2010-01-21 14:36:03 +0800

    move size estimation

---
+++ src/estimation/sizeEstimation/SizeEstimationWizardDialog.java
@@ -0,0 +1,15 @@
+package estimation.sizeEstimation;
+
+import org.eclipse.jface.wizard.IWizard;
+import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.swt.widgets.Shell;
+
+public class SizeEstimationWizardDialog extends WizardDialog{
+
+	
+	public SizeEstimationWizardDialog(Shell parentShell, IWizard newWizard) {
+		super(parentShell, newWizard);
+		// TODO Auto-generated constructor stub
+	}
+
+}
