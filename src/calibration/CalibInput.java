87ad19732184b4d2b85eebc0e72e8234274996a3
林中鹏 <lin.zhp@gmail.com>
2010-03-26 18:02:20 +0800

    start to calibrate

--- src/calibration/CalibInput.java
+++ src/calibration/CalibInput.java
@@ -3,8 +3,12 @@ package calibration;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.IToolBarManager;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.MessageBox;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableColumn;
@@ -12,14 +16,38 @@ import org.eclipse.swt.widgets.TableItem;
 
 import data.database.dataAccess.NodeBasicInfoAccess;
 import data.database.dataEntities.Entity;
+import data.file.COCOMOProperties;
 
 import gui.tabs.ParameterArea;
 
 public class CalibInput extends ParameterArea {
 	public static final int ID = -2;
+	private Label aLabel, bLabel;
 	public CalibInput(Composite parent){
 		super(parent, null);
 		form.setText("参数校准");
+		IToolBarManager toolBarManager = form.getToolBarManager();
+		toolBarManager.add(new Action() {
+			@Override
+			public void run() {
+				
+			}
+		});
+		showParams();
+		chooseHistoricalProjects();
+	}
+
+	private void showParams() {
+		Composite panel = toolkit.createComposite(form.getBody());
+		panel.setLayout(new GridLayout(2, false));
+		toolkit.createLabel(panel, "A=");
+		aLabel = toolkit.createLabel(panel, COCOMOProperties.readValue("A"));
+		toolkit.createLabel(panel, "B=");
+		bLabel = toolkit.createLabel(panel, COCOMOProperties.readValue("B"));
+	}
+	
+	private void chooseHistoricalProjects() {
+		toolkit.createLabel(form.getBody(), "请选择历史项目");
 		Table table = new Table(form.getBody(), SWT.BORDER | SWT.CHECK);
 		table.setLinesVisible(true);
 		table.setHeaderVisible(true);
--- src/calibration/CalibTest.java
+++ src/calibration/CalibTest.java
@@ -9,14 +9,14 @@ public class CalibTest {
 	 */
 	public static void main(String[] args) {
 		SimpleRegression regression = new SimpleRegression();
-		regression.addData(Math.log(134.47), Math.log(1854.55/1.89)-29.28*Math.log(134.47));
-		regression.addData(Math.log(132), Math.log(258.51/0.49)-16.72*Math.log(132));
-		regression.addData(Math.log(44.03), Math.log(201/1.06)-22.48*Math.log(44.03));
-		regression.addData(Math.log(3.57), Math.log(58.87/5.05)-18.19*Math.log(3.57));
-		regression.addData(Math.log(380.8), Math.log(9661.02/3.05)-26.77*Math.log(380.8));
-		regression.addData(Math.log(980), Math.log(7021.28/0.92)-25.21*Math.log(980));
-		regression.addData(Math.log(11.186), Math.log(91.67/2.45)-23.5*Math.log(11.186));
-		regression.addData(Math.log(61.56), Math.log(689.66/2.38)-26.48*Math.log(61.56));
+		regression.addData(Math.log(134.47), Math.log(1854.55/1.89)-0.01*29.28*Math.log(134.47));
+		regression.addData(Math.log(132), Math.log(258.51/0.49)-0.01*16.72*Math.log(132));
+		regression.addData(Math.log(44.03), Math.log(201/1.06)-0.01*22.48*Math.log(44.03));
+		regression.addData(Math.log(3.57), Math.log(58.87/5.05)-0.01*18.19*Math.log(3.57));
+		regression.addData(Math.log(380.8), Math.log(9661.02/3.05)-0.01*26.77*Math.log(380.8));
+		regression.addData(Math.log(980), Math.log(7021.28/0.92)-0.01*25.21*Math.log(980));
+		regression.addData(Math.log(11.186), Math.log(91.67/2.45)-0.01*23.5*Math.log(11.186));
+		regression.addData(Math.log(61.56), Math.log(689.66/2.38)-0.01*26.48*Math.log(61.56));
 		System.out.println(regression.getIntercept());
 		System.out.println(regression.getSlope());
 //		double sum=0;
